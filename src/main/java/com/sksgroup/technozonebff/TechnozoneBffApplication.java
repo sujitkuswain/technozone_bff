2c18f4dd42463f660b80bacfebedeec7cf0d7daf
Sujit Kumar Swain <sujitkuswain@gmail.com>
2024-01-21 16:12:06 +0100

    Initial commit

---
+++ src/main/java/com/sksgroup/technozonebff/TechnozoneBffApplication.java
@@ -0,0 +1,13 @@
+package com.sksgroup.technozonebff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnozoneBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnozoneBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sksgroup/technozonebff/TechnozoneBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sksgroup.technozonebff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnozoneBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
