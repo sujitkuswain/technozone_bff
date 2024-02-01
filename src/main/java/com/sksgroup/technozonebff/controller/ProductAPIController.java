1e8638475ab0518379e595123f40533fec301f6f
Sujit Kumar Swain <sujitkuswain@gmail.com>
2024-02-01 15:10:35 +0100

    test direct url

--- src/main/java/com/sksgroup/technozonebff/controller/ProductAPIController.java
+++ src/main/java/com/sksgroup/technozonebff/controller/ProductAPIController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/product")
+//@RequestMapping(path = "api/v1/product")
 public class ProductAPIController {
 
     @Autowired
